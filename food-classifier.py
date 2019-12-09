bc049c8ff467a8bd7acc5cc1870cb48f52ffc96d
TWash8 <twashi12@gmu.edu>
2019-12-09 00:29:32 -0500

    Added skeleton methods for (hopefully) rest of program.

--- food-classifier.py
+++ food-classifier.py
@@ -12,27 +12,22 @@ from PIL import Image
 from skimage import filters
 from skimage.color.adapt_rgb import adapt_rgb, each_channel, hsv_value
 
-#This is all of our images we train off of
-#Open them and make it pretty
-trainData = open('./food41/meta/meta/train.txt', 'r')
-train_foods = trainData.read().splitlines()
+#Possible helper method that can link each image to its class
+#Training model (for now)
+def classes_and_images():
+    return None
 
-#Takes the image and turns it into multidimensional list
-#that is based off of the image's width and height
-    '''
-def toMatrix(location):
-    image = Image.open('./food41/images/'+location + '.jpg', 'r')
-    width, height = image.size
-    pix_matrix = []
-    pix_val = list(image.getdata())
-    for x in range(0, height):
-        temp = []
-        for y in range(x, x + width):
-            temp.append(pix_val[y])
-        pix_matrix.append(temp)
-    return pix_matrix
-    '''
+#Set up our CNN training model
+def training_model():
+    return None
 
+#Evaluate the results of our training model by running some predictions
+def evaluation():
+    return None
+
+#Show the results of our predictions via confusion matrix
+def confusion():
+    return None
 #Takes the image we loaded and standardize scaling, dimensions, etc.
 #Preprocessing for training set (for now)
 def standardize(image, min):
@@ -41,17 +36,37 @@ def standardize(image, min):
         width, height, z = image.shape
         if (width < min):
             x = int((float(height)*float(min/float(width))))
-            new_image = imresize(new_image, (min, x))
+            new_image2 = imresize(new_image, (min, x))
         elif(height< min):
             yy = int((float(width)*float(min/float(height))))
-            new_image = imresize(new_image, (min, y))
+            new_image2 = imresize(new_image, (min, y))
 
     except:
         print("Image error. Skipping...")
-    return new_image
+
+    gen = ImageDataGenerator(zoom_range = [.9,1],
+    horizontal_flip=false,
+    vertical_flip=false,
+    featurewise_center=False,
+    samplewise_center=False,
+    featurewise_std_normalization = False,
+    samplewise_std_normalization=False,
+    channel_shift_range=0,
+    rotation_range = 0,
+    fill_mode="reflect")
+    
+    gen.config["random_crop_size"] = (min, min)
+    trainingModel = gen
+    return new_image2
 
 def main():
     resized_array = []
+    class_array = []
+    trainData = open('./food41/meta/meta/train.txt', 'r')
+    train_foods = trainData.read().splitlines()
+    for line in train_foods:
+        line2 = line.split("/")
+        class.append(line2[0])
     print(train_foods)
     print("HELLOLOLOL0 CHIKCHENS")
     for line in train_foods:
